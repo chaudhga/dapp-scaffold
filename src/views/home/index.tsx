12c19d9553221494a5b2e32c274f56f4e11dccde
Gaurav C <gaurav.chaudhari@gmail.com>
2023-06-29 19:40:43 +0800

    version bump

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -31,7 +31,7 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v0.0.1</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
           NFT Fractionizer
         </h1>
