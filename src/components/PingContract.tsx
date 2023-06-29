ace7b614758878d99b65524c7c395e82363b719b
Gaurav C <gaurav.chaudhari@gmail.com>
2023-06-29 12:27:10 +0800

    added pingContract for debugging

---
+++ src/components/PingContract.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { Connection, PublicKey } from '@solana/web3.js';
+
+const PingContract: React.FC = () => {
+  const defaultProgramId = 'YourDefaultProgramIdHere';
+  const defaultNetwork = 'https://api.mainnet-beta.solana.com';
+
+  const [programIdInput, setProgramIdInput] = useState<string>(defaultProgramId);
+  const [networkInput, setNetworkInput] = useState<string>(defaultNetwork);
+  const [isPinging, setIsPinging] = useState<boolean>(false);
+  const [pingResult, setPingResult] = useState<string>('');
+
+  const handlePing = async () => {
+    setIsPinging(true);
+    const connection = new Connection(networkInput, 'confirmed');
+    const programId = new PublicKey(programIdInput);
+
+    try {
+      await connection.getAccountInfo(programId);
+      setPingResult('Smart contract is reachable');
+    } catch (error) {
+      setPingResult('Smart contract is not reachable or does not exist');
+    } finally {
+      setIsPinging(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Ping Solana Smart Contract</h2>
+      <label htmlFor="programIdInput">Program ID:</label>
+      <input
+        id="programIdInput"
+        type="text"
+        value={programIdInput}
+        onChange={(e) => setProgramIdInput(e.target.value)}
+      />
+      <label htmlFor="networkInput">Network:</label>
+      <input
+        id="networkInput"
+        type="text"
+        value={networkInput}
+        onChange={(e) => setNetworkInput(e.target.value)}
+      />
+      <button onClick={handlePing} disabled={isPinging}>
+        {isPinging ? 'Pinging...' : 'Ping'}
+      </button>
+      <p>{pingResult}</p>
+    </div>
+  );
+};
+
+export default PingContract;
